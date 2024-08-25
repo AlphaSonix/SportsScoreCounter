3e8c27b8346cb0b6bf6a7aeace8be8f2f0476941
Jermain Dunkley <jermaindunkley@unknown.home>
2024-08-25 20:11:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+let scoreDisplayHome = document.getElementById("homeScore");
+let scoreDisplayGuest = document.getElementById("guestScore");
+//console.log(scoreDisplay);
+let totalScoreHome = 0;
+let totalScoreGuest = 0; 
+
+scoreDisplayGuest.textContent = 0;
+scoreDisplayHome.textContent = 0;
+
+function addOneHome() {
+    totalScoreHome += 1;
+    scoreDisplayHome.textContent = totalScoreHome;
+}
+
+function addTwoHome() {
+    totalScoreHome += 2;
+    scoreDisplayHome.textContent = totalScoreHome;
+}
+
+function addThreeHome() {
+    totalScoreHome += 3;
+    scoreDisplayHome.textContent = totalScoreHome;
+}
+
+function addOneGuest() {
+    totalScoreGuest += 1;
+    scoreDisplayGuest.textContent = totalScoreGuest;
+}
+
+function addTwoGuest() {
+    totalScoreGuest += 2;
+    scoreDisplayGuest.textContent = totalScoreGuest;
+}
+
+function addThreeGuest() {
+    totalScoreGuest += 3;
+    scoreDisplayGuest.textContent = totalScoreGuest;
+}
+
+function resetHome() {
+    totalScoreHome = 0;
+    scoreDisplayHome.textContent = 0;
+}
+
+function resetGuest() {
+    totalScoreGuest = 0;
+    scoreDisplayGuest.textContent = 0;
+}
