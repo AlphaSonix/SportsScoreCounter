cdfe636b33103e13385831c17b36dd980ffeea82
Jermain Dunkley <jermaindunkley@jermains-air.home>
2024-08-27 10:16:37 +0100

    changes color if a team is winning

--- index.js
+++ index.js
@@ -10,31 +10,37 @@ scoreDisplayHome.textContent = 0;
 function addOneHome() {
     totalScoreHome += 1;
     scoreDisplayHome.textContent = totalScoreHome;
+    isWinning();
 }
 
 function addTwoHome() {
     totalScoreHome += 2;
     scoreDisplayHome.textContent = totalScoreHome;
+    isWinning();
 }
 
 function addThreeHome() {
     totalScoreHome += 3;
     scoreDisplayHome.textContent = totalScoreHome;
+    isWinning();
 }
 
 function addOneGuest() {
     totalScoreGuest += 1;
     scoreDisplayGuest.textContent = totalScoreGuest;
+    isWinning();
 }
 
 function addTwoGuest() {
     totalScoreGuest += 2;
     scoreDisplayGuest.textContent = totalScoreGuest;
+    isWinning();
 }
 
 function addThreeGuest() {
     totalScoreGuest += 3;
     scoreDisplayGuest.textContent = totalScoreGuest;
+    isWinning();
 }
 
 function resetHome() {
@@ -46,4 +52,18 @@ function resetGuest() {
     totalScoreGuest = 0;
     scoreDisplayGuest.textContent = 0;
 }
+
+function isWinning() {
+    if (totalScoreHome > totalScoreGuest) {
+        scoreDisplayHome.style.color = "green";
+        scoreDisplayGuest.style.color = "deeppink";
+    } else if (totalScoreGuest > totalScoreHome) {
+        scoreDisplayGuest.style.color = "green";
+        scoreDisplayHome.style.color = "deeppink";
+    } else {
+        scoreDisplayHome.style.color = "deeppink";
+        scoreDisplayGuest.style.color = "deeppink";
+    }
+}
+
 
